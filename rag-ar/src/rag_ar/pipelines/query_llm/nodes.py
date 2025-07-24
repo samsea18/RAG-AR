40adf11054d4a4800d0ad232d7ecb60107e8cf60
Sam Sea <samsea@Sams-MacBook-Pro.local>
2025-07-24 20:48:15 +0800

    Updated query_llm with kedro config loader

--- rag-ar/src/rag_ar/pipelines/query_llm/nodes.py
+++ rag-ar/src/rag_ar/pipelines/query_llm/nodes.py
@@ -2,8 +2,16 @@ from typing import List, Dict
 from langchain.vectorstores import FAISS
 from llm_clients.openrouter_client import call_openrouter
 
+from kedro.config import OmegaConfigLoader
+from kedro.framework.context import KedroContext
+from kedro.framework.project import settings
+
 
 def query_node(queries: List[str], vectorstore: FAISS, top_k: int, model: str, api_key: str) -> List[Dict]:
+
+    conf_loader = OmegaConfigLoader(conf_source=settings.CONF_SOURCE)
+    api_key = conf_loader['credentials']['openrouter']['api_key']
+
     results = []
     for q in queries:
         rel_docs = vectorstore.similarity_search(q, k=top_k)
